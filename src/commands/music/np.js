892b5358dca4162f2109d2965690e3c9984d89f2
Andronic123 <fotopoulos.andrew@gmail.com>
2022-11-02 11:20:46 +0200

    added skip and np command, fixed play command audioresource states

--- src/commands/music/np.js
+++ src/commands/music/np.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+const { getVoiceConnection } = require("@discordjs/voice")
 
 module.exports = {
   data: new SlashCommandBuilder().setName("np").setDescription("WIP"),
@@ -6,10 +8,30 @@ module.exports = {
     const message = await interaction.deferReply({
       fetchReply: true,
     });
-    return;
+
+    let Queue = fs.readFileSync("queue.json", "utf8");
+    var noQueue = "Nothing is playing men";
+    if (!Queue || getVoiceConnection(interaction.guild.id) == undefined)
+      return interaction.editReply({
+        content: noQueue,
+      });
+
+    Queue = JSON.parse(Queue);
+
+    let NowPlaying = new EmbedBuilder()
+        .setTitle("🎶 **Now Playing** 🎶")
+        .setColor("DarkGreen")
+        .setThumbnail(Queue[0].thumb)
+        .addFields([
+          {
+            name: `Song Information:`,
+            value: `[${Queue[0].title}](${Queue[0].url})\n\n**Channel: __${Queue[0].chann}__\nLength: __${Queue[0].time}__**`,
+            inline: false,
+          },
+        ]);
 
     await interaction.editReply({
-      content: newMessage,
+      embeds: [NowPlaying],
       ephemeral: true,
     });
   },
