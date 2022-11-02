07c4b9c0c63dc7a7e22da7785bf3cc4cf9035047
Andronic123 <fotopoulos.andrew@gmail.com>
2022-11-02 11:24:34 +0200

    forgor

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const { getVoiceConnection } = require("@discordjs/voice");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("skip").setDescription("WIP"),
+  data: new SlashCommandBuilder().setName("skip").setDescription("Skips current song"),
   async execute(interaction, client) {
     const message = await interaction.deferReply({
       fetchReply: true,
