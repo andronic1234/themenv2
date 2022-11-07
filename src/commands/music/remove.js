36d5b30c52e835665b223101642e0f4967cfed48
Andronic123 <fotopoulos.andrew@gmail.com>
2022-11-07 11:27:34 +0200

    Update remove.js

--- src/commands/music/remove.js
+++ src/commands/music/remove.js
@@ -3,13 +3,24 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("remove")
-    .setDescription("WIP"),
+    .setDescription("WIP")
+    .addStringOption((option) =>
+    option
+      .setName("song")
+      .setDescription("Specify number of song in Queue")
+      .setRequired(true)
+  ),
   async execute(interaction, client) {
     const message = await interaction.deferReply({
       fetchReply: true,
     });
-
-    return
+    let input = interaction.options._hoistedOptions[0].value;
+    let isnum = /^\d+$/.test(input);
+    if (isnum == true) {
+        newMessage = `Received number: ${input}`
+    } else {
+        newMessage = `${input} is not a number men`
+    }
 
     await interaction.editReply({
       content: newMessage,
