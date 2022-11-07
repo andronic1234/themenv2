eae310d70dcfeff12f9bd0b3586409bbd48931fe
Andronic123 <fotopoulos.andrew@gmail.com>
2022-11-07 13:03:58 +0200

    Update clear.js

--- src/commands/tools/clear.js
+++ src/commands/tools/clear.js
@@ -1,42 +1,58 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Shows the Bot's ping")
-    .addStringOption((option) =>
+    .setName("clear")
+    .setDescription("Clears the specified amount of messages")
+    .addIntegerOption((option) =>
       option
         .setName("messages")
         .setDescription("Specify number of messages you want to remove")
         .setRequired(true)
+    )
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("Specify a user")
+        .setRequired(false)
     ),
   async execute(interaction, client) {
-    const message = await interaction.deferReply({
-      fetchReply: true,
+    // if (!interaction.member.permissions.has("MANAGE_MESSAGES")) {
+    //   return interaction.reply("You don't have the permission for that");
+    // }
+    // console.log(interaction.options._hoistedOptions)
+    const { channel, options } = interaction;
+
+    const amount = interaction.options._hoistedOptions[0].value;
+    var target = interaction.options._hoistedOptions[1]
+    if(target) {
+        target = interaction.options._hoistedOptions[1].user;
+    }
+
+    const messages = await channel.messages.fetch({
+      limit: amount + 1,
     });
 
-    if (!interaction.member.permissions.has("MANAGE_MESSAGES")) {
-        return interaction.reply('You don\'t have the permission for that');
-    }
-    let input = interaction.options._hoistedOptions[0].value;
-    let isnum = /^\d+$/.test(input);
-    if (isnum(input) == false || input < 0) {
-      newMessage = "Please specify the number of messages to clear.";
-    } else if ((input = 0)) {
-      newMessage = "are u retarded men";
-    } else if (input > 100) {
-      newMessage = "I can only delete 100 messages at a time men";
-    } else {
-      newMessage`Deleted **${input}** messages.`;
-    }
-    await interaction.channel.messages
-      .fetch({ limit: input })
-      .then((messages) => {
-        interaction.channel.bulkDelete(messages);
+    const res = new EmbedBuilder().setColor("DarkAqua");
+
+    if (target) {
+      let i = 0;
+      const filtered = [];
+      (await messages).filter((msg) => {
+        if (msg.author.id === target.id && amount > i) {
+          filtered.push(msg);
+          i++;
+        }
       });
-    await interaction.editReply({
-      content: newMessage,
-      ephemeral: true,
-    });
+      await channel.bulkDelete(filtered).then((messages) => {
+        res.setDescription(`Deleted ${messages.size} messages from ${target}.`);
+        interaction.reply({ embeds: [res] });
+      });
+    } else {
+      await channel.bulkDelete(amount, true).then((messages) => {
+        res.setDescription(`Deleted ${messages.size} messages from the channel.`);
+        interaction.reply({ embeds: [res] });
+      });
+    }
   },
 };
